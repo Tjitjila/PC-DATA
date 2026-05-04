b9e907ed43b95850067d583e3eee04dddf3315ea
Tjitjila Netshiungani <tsmokono@gmail.com>
2026-05-04 10:53:10 +0100

    Questions 5-10

--- PC_DATA_QUESTIONS/pc_sales_sql_questions.sql
+++ PC_DATA_QUESTIONS/pc_sales_sql_questions.sql
@@ -15,16 +15,16 @@ from dbo.[pc_data]
 
 -- 2. Count the number of sales per Continent.
 
-select Continent,
-count (*) as Number_of_sales_per_continent
-from dbo.[pc_data]
-group by continent
+Select Continent, 
+Count (Continent) As Number_of_sales
+from dbo.pc_data
+group by Continent
 
 -- 3. Count the number of sales per Country or State.
 
-select Country_or_State,
-count (*) as Number_of_sales_per_state
-from dbo.[pc_data]
+Select Country_or_State, 
+Count (Country_or_State) As Number_of_sales  --count the number of rows that have the same province
+from dbo.pc_data
 group by Country_or_State
 
 -- 4. List all distinct Shop Name values.
@@ -33,13 +33,42 @@ Select distinct (Shop_Name)
 from dbo.pc_data
 
 -- 5. Find the average Sale Price.
-
+SELECT AVG(Sale_Price) 
+FROM dbo.pc_data
 
 -- 6. Find the highest and lowest Sale Price.
+
+SELECT MIN(Sale_Price) As lowest_price
+FROM dbo.pc_data
+
+SELECT MAX(Sale_Price) As Highest_price
+FROM dbo.pc_data
+
 -- 7. Count the number of sales by Payment Method.
+
+Select Payment_Method, 
+Count (Payment_Method) As Number_of_sales_per_payment_method
+from dbo.pc_data
+group by Payment_Method
+
 -- 8. Count the number of sales by Channel (Online vs Offline).
+
+Select Channel, 
+Count (Channel) As Number_of_sales_per_channel
+from dbo.pc_data
+group by Channel
+
 -- 9. Count the number of sales by Priority level.
+
+Select Priority, 
+Count (Priority) As Number_of_sales_per_channel
+from dbo.pc_data
+group by Priority
+
 -- 10. Count the number of distinct PC Make values.
+
+Select distinct (PC_Model)
+From dbo.pc_data
 
 -- INTERMEDIATE QUESTIONS
 
